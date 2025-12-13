bf9d58d00931e37d70251a3779480507a359387a
kothman02 <142905817+kothman02@users.noreply.github.com>
2025-12-13 11:00:29 +0300

    Move public files to root

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php
+require_once "../includes/db.php";
+
+// get values
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+// insert
+$sql = "INSERT INTO messages (name, email, phone, message) VALUES (?, ?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ssss", $name, $email, $phone, $message);
+$stmt->execute();
+
+// return back to form with success flag
+header("Location: index.php?sent=1");
+exit;
--- public/test_db.php
+++
@@ -1,5 +0,0 @@
-<?php
-//http://localhost/m_a_mix/public/test_db.php
-require_once "../includes/db.php";
-
-echo "Database connection OK for M A MIX";
