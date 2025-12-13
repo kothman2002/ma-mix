a47e12eac41e15d5b08588ac488eb071c8f99d70
kothman02 <142905817+kothman02@users.noreply.github.com>
2025-12-14 00:42:30 +0300

    Fix include paths

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ?>
-<?php include "../includes/header.php"; ?>
-<?php include "../includes/navbar.php"; ?>
-<?php include "../includes/hero.php"; ?>
-<?php include "../includes/about.php"; ?>
-<?php include "../includes/services.php"; ?>
-<?php include "../includes/fields-slider.php"; ?>
-<?php include "../includes/services_section.php"; ?>
-<?php include "../includes/why_us.php"; ?>
-<?php include "../includes/partners.php"; ?> 
-<?php include "../includes/contact.php"; ?> 
-<?php include "../includes/footer.php"; ?>
+<?php include "includes/header.php"; ?>
+<?php include "includes/navbar.php"; ?>
+<?php include "includes/hero.php"; ?>
+<?php include "includes/about.php"; ?>
+<?php include "includes/services.php"; ?>
+<?php include "includes/fields-slider.php"; ?>
+<?php include "includes/services_section.php"; ?>
+<?php include "includes/why_us.php"; ?>
+<?php include "includes/partners.php"; ?> 
+<?php include "includes/contact.php"; ?> 
+<?php include "includes/footer.php"; ?>
