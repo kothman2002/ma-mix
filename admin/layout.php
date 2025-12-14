108f76cb3a0fc40573c1c534a26647e9cc9678dd
kothman02 <142905817+kothman02@users.noreply.github.com>
2025-12-15 00:12:36 +0300

    Update why us section layout and text width

--- admin/layout.php
+++ admin/layout.php
@@ -70,9 +70,8 @@
 
 <!-- SIDEBAR -->
 <div class="sidebar text-center p-3">
-
-<img src="/assets/img/logo.png" alt="Logo">
-         
+    
+<img src="../assets/img/logo.png" alt="Logo"
          style="width:130px; margin-bottom:20px; filter: drop-shadow(0 0 3px rgba(0,0,0,0.2));">
 
     <a href="dashboard.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'dashboard.php' ? 'active' : '' ?>">
