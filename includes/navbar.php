769ab975aa7a16651846f0f61b8b036c926a3bfa
kothman02 <142905817+kothman02@users.noreply.github.com>
2025-12-15 02:13:01 +0300

    Update why us section layout and text width

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,15 +1,12 @@
 <div class="top-bar">
-
-    <img src="assets/img/m_mix_logo.png" class="logo">
-
-    <div class="nav-links" id="navLinks">
+    <div class="nav-links">
         <a href="#about">من نحن</a>
         <a href="#services">خدماتنا</a>
         <a href="#fields-slider-section">مجالاتنا</a>
         <a href="#services-section">ماذا نقدّم</a>
-        <a href="#why-us-section">لماذا نحن</a>
-        <a href="#partners-section">شركائنا بالنجاح</a>
-        <a href="#contact-section">تواصل معنا</a>
+    <a href="#why-us-section">لماذا نحن</a>
+    <a href="#partners-section">شركائنا بالنجاح </a>
+    <a href="#contact-section">تواصل معنا</a>
     </div>
-    <div class="menu-toggle" onclick="toggleMenu()">☰</div>
+    <img src="assets/img/m_mix_logo.png" class="logo">
 </div>
