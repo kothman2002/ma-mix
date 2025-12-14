c48b78805959390866b9dcad9f6a89805e911275
kothman02 <142905817+kothman02@users.noreply.github.com>
2025-12-15 02:42:09 +0300

    Update why us section layout and text width

--- includes/header.php
+++ includes/header.php
@@ -13,6 +13,8 @@ if (!isset($_SESSION['visited'])) {
 <html lang="ar" dir="rtl">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>M A MIX</title>
 
     <!-- Favicon -->
