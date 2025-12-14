2c322888c1492b596e79298899d5c2ca7798cc6d
kothman02 <142905817+kothman02@users.noreply.github.com>
2025-12-15 00:43:31 +0300

    Update why us section layout and text width

--- includes/navbar.php
+++ includes/navbar.php
@@ -10,16 +10,5 @@
 
     </div>
     <img src="assets/img/m_mix_logo.png" class="logo">
-        <div class="mobile-menu-btn" onclick="toggleMobileMenu()">☰</div>
-
-</div>
-<!-- قائمة الموبايل -->
-<div id="mobileMenu" class="mobile-menu">
-    <a href="#about" onclick="toggleMobileMenu()">من نحن</a>
-    <a href="#services" onclick="toggleMobileMenu()">خدماتنا</a>
-    <a href="#fields-slider-section" onclick="toggleMobileMenu()">مجالاتنا</a>
-    <a href="#services-section" onclick="toggleMobileMenu()">ماذا نقدّم</a>
-    <a href="#why-us-section" onclick="toggleMobileMenu()">لماذا نحن</a>
-    <a href="#partners-section" onclick="toggleMobileMenu()">شركائنا بالنجاح</a>
-    <a href="#contact-section" onclick="toggleMobileMenu()">تواصل معنا</a>
+    
 </div>
